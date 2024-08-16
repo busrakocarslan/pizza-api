b72866c992457d9276178400c78dc4ac23bbded3
busra <kocarslanbusrab@gmail.com>
2024-08-16 17:03:31 +0300

    multer

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -9,15 +9,20 @@
 
 const multer = require('multer')
 
+// module.exports = multer({
+//     // dest: './uploads',
+//     storage: multer.diskStorage({
+//         destination: './uploads',
+//         filename: function (req, file, returnCallback) {
+//             // console.log('file', file)
+//             // returnCallback(error, fileName)
+//             // returnCallback(null, file.originalname)
+//             returnCallback(null, Date.now() + '_' + file.originalname)// hata göndermiyoruz ve dosya isminiş ayarlıyoruz. dosya ismi aybı olursa üst üste bastığı içinde date.now ile timestamp ekliyoruz.
+//         }
+//     })
+// })
+// const multer = require('multer');
+
 module.exports = multer({
-    // dest: './uploads',
-    storage: multer.diskStorage({
-        destination: './uploads',
-        filename: function (req, file, returnCallback) {
-            // console.log('file', file)
-            // returnCallback(error, fileName)
-            // returnCallback(null, file.originalname)
-            returnCallback(null, Date.now() + '_' + file.originalname)// hata göndermiyoruz ve dosya isminiş ayarlıyoruz. dosya ismi aybı olursa üst üste bastığı içinde date.now ile timestamp ekliyoruz.
-        }
-    })
-})
+    storage: multer.memoryStorage(), // Dosyalar bellekte tutulacak
+});
